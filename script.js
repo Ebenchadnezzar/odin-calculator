74360603c04a520ac53021d8781b3cdd7caeccb0
Ebenchadnezzar <168885806+Ebenchadnezzar@users.noreply.github.com>
2025-06-28 17:01:06 -0700

    Adds better operate functionality
    Now, if you type an operand, then a operator, then
    an operand, and finally another operator, it will
    evaluate the initial expression, save it as the
    first operand, and let you continue with the
    expression. Also, if you evaluate an expression,
    then type a number, it will replace the result of
    the previous expression, but, if you instead type
    an operator, it will treat the result of the
    previous expression as the first operand of a new
    expression.

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ onStart();
 
 function onStart() {
     let mainButtons = document.querySelectorAll(".mainButtons button, .mathButtons button");
-
+    console.log(display.textContent);
     // Set up the main buttons (Numbers, decimal, equal, and math buttons)
     mainButtons.forEach((element) => {
         element.addEventListener("click", onMainButtonPress);
     });
 }
 
-function operate(a, symbol, b) {
+function operate() {
     let result;
-    a = +a; // They're stored as strings
-    b = +b;
+    operand1 = +operand1; // They're stored as strings
+    operand2 = +operand2;
     
-    switch (symbol) {
+    switch (operator) {
         case "/":
-            if (b === 0) { result = 0; }
-            else { result = a / b; }
+            if (operand2 === 0) { result = 0; }
+            else { result = operand1 / operand2; }
             break;
         case "X":
-            result = a * b;
+            result = operand1 * operand2;
             break;
         case "-":
-            result = a - b;
+            result = operand1 - operand2;
             break;
         case "+":
-            result = a + b;
+            result = operand1 + operand2;
             break;
     }
 
@@ -62,6 +62,13 @@ function onMainButtonPress(e) {
         case "8":
         case "9":
         case ".":
+            if (operand1 === null) {
+                display.textContent = symbol;
+            }
+            else {
+                display.textContent += symbol;
+            }
+
             if (operator === null) {
                 if (operand1 === null) { operand1 = symbol; }
                 else { operand1 += symbol; }
@@ -70,19 +77,35 @@ function onMainButtonPress(e) {
                 if (operand2 === null) { operand2 = symbol; }
                 else { operand2 += symbol; }
             }
-            display.textContent += symbol;
             break;
         case "/":
         case "X":
         case "-":
         case "+":
-            if (operand1 != null && operator == null) {
+            if (operand1 !== null && operator === null) {
+                // Standard behavior, if you hit a number then hit a symbol
+                operator = symbol;
+                display.textContent += symbol;
+            }
+            else if (operand1 !== null & operator !== null && operand2 !== null) {
+                // If you've arleady typed a full expression, but then type
+                // another operator, it calculates the first expression,
+                // at treats it as the first operand.
+                operate();
+                operand1 = display.textContent;
+                operator = symbol;
+                display.textContent += symbol;
+            }
+            else if (operand1 === null && operator === null && operand2 === null && display.textContent !== "") {
+                // If you've just completed an operation and press an
+                // operator, it will treat the result as the first operand
+                operand1 = display.textContent;
                 operator = symbol;
                 display.textContent += symbol;
             }
         case "=":
             if (operand1 != null && operator != null && operand2 != null) {
-                operate(operand1, operator, operand2);
+                operate();
             }
     }
 }
